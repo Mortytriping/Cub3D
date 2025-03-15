33caa47498969351278cf6afc453a4733c60ab94
Arthur Pouessel <apouesse@student.42.fr>
2025-03-15 12:42:29 +0100

    developpement de la structure du projet

--- srcs/Parsing/parsing.c
+++ srcs/Parsing/parsing.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apouesse <apouesse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/15 10:52:36 by apouesse          #+#    #+#             */
-/*   Updated: 2025/03/15 12:38:37 by apouesse         ###   ########.fr       */
+/*   Created: 2025/03/15 12:40:58 by apouesse          #+#    #+#             */
+/*   Updated: 2025/03/15 12:41:07 by apouesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUBD3D_H
-# define CUBD3D_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-# include <math.h>
-
-typedef struct s_colors
-{
-	int	mur;
-} t_colors;
-
-typedef struct s_player
-{
-	float	px;
-	float	py;
-} t_player;
-
-#endif
+#include "../../includes/cub3d.h"
